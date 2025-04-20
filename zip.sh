fe79238492a8d84b864423317f7a172cdaf70e3c
Jay Kumar <90331269+BreadCatto@users.noreply.github.com>
2025-04-20 23:54:21 +0530

    Update zip.sh

--- zip.sh
+++ zip.sh
@@ -3,5 +3,13 @@ apt update
 apt install unzip
 apt install axel
 echo "downloading zip"
+cd /home/container/series
 axel -n 10 {{ZIP_URL}}
+echo "extracting zip"
+FILENAME=$(basename "$ZIP_URL")
+if [[ -f "$FILENAME" && "$FILENAME" == *.zip ]]; then
+    unzip "$FILENAME"
+else
+    echo "Downloaded file is not a ZIP or doesn't exist: $FILENAME"
+fi
 echo "done"
